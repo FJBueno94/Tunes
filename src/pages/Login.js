b008e75fcadcf31658c83f4ad6be1be00c0acb7f
Fernando Bueno <fernando_jordi@hotmail.com>
2022-04-19 20:52:36 -0300

    adiciona o requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,98 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Carregando from '../components/Carregando';
 
 class Login extends React.Component {
-  render() {
-    return (
-      <div data-testid="page-login">
-        .
-      </div>
-    );
+  constructor() {
+    super();
+    this.verifyLogin = this.verifyLogin.bind(this);
+
+    this.state = {
+      loginName: '',
+      disabledButton: true,
+      loading: false,
+      redirect: false,
+    };
   }
+
+    verifyUser = async () => {
+      const { loginName } = this.state;
+      this.setState({
+        loading: true,
+      });
+      await createUser({ name: loginName });
+      this.setState({
+        loading: false,
+        redirect: true,
+      });
+    }
+
+    verifyLogin({ target }) {
+      const { name } = target;
+      const value = target.type === 'checkbox' ? target.checked : target.value;
+      this.setState({
+        [name]: value,
+      });
+
+      const { loginName } = this.state;
+      const minChar = 2;
+      if (loginName.length >= minChar) {
+        this.setState({
+          disabledButton: false,
+        });
+      } else {
+        this.setState({
+          disabledButton: true,
+        });
+      }
+    }
+
+    render() {
+      const {
+        loginName,
+        disabledButton,
+        loading,
+        redirect,
+      } = this.state;
+
+      if (redirect) {
+        return (
+          <Redirect to="/search" />
+        );
+      }
+      return (
+        <div data-testid="page-login">
+          {
+            loading ? <Carregando /> : (
+              <form>
+                <label htmlFor="login">
+                  Nome:
+                  <input
+                    type="text"
+                    id="login"
+                    name="loginName"
+                    value={ loginName }
+                    placeholder="Insira seu nome"
+                    onChange={ this.verifyLogin }
+                    data-testid="login-name-input"
+                  />
+                </label>
+                <button
+                  type="button"
+                  id="login"
+                  data-testid="login-submit-button"
+                  disabled={ disabledButton }
+                  onClick={ this.verifyUser }
+                >
+                  Entrar
+                </button>
+              </form>
+            )
+          }
+        </div>
+      );
+    }
 }
 
 export default Login;
