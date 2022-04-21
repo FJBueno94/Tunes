5b852ab7d298c52d57a2d4ee21087d800e96f0cb
Fernando Bueno <fernando_jordi@hotmail.com>
2022-04-21 13:54:22 -0300

    adiciona o requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Carregando from './Carregando';
 
@@ -33,9 +34,20 @@ export default class Header extends Component {
       <header data-testid="header-component">
         {
           loading ? (<Carregando />) : (
-            <h3 data-testid="header-user-name">
-              { usuario }
-            </h3>)
+            <>
+              <div>
+                <h4>Seja Bem vindo</h4>
+                <h3 data-testid="header-user-name">
+                  {usuario}
+                </h3>
+              </div>
+              <div>
+                <Link to="/search" data-testid="link-to-search">Pesquisar</Link>
+                <Link to="/favorites" data-testid="link-to-favorites">Favoritos</Link>
+                <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
+              </div>
+
+            </>)
         }
       </header>
     );
